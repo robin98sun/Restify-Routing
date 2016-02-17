32ebf7ea919a5e86f43db342910af4de79780ff4
Sun Lin <sunlin@goyoo.com>
2016-02-17 21:11:13 +0800

    Fixed bugs

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ let legalMethods = ['get', 'put', 'post', 'del', 'patch', 'head', 'delete'];
 
 let routerPrototype = {
     legalMethods: legalMethods,
-
-    all(path){
-        let fp = path || '/';
-        if(fp === '*') fp = '/'
-        if(!this.superActions) this.superActions = {};
-        if(!this.superActions[fp])this.superActions[fp] = [];
+    debugLog (mesg){
+        if(!this.debug) return;
+        console.log(mesg);
+    },
+    all(pattern){
+        let fp = pattern || '*';
+        fp = fp.replace( /[\*]+/g, '*');
+        if(!this.middlewares) this.middlewares = [];
         let cbList = Array.prototype.slice.call(arguments, 1);
-        if(cbList && cbList.length > 0){
-            for(let cb of cbList){
-                if(typeof cb === 'function')
-                    this.superActions[fp].push(cb)
-            }
+        let targetAction = null;
+        this.middlewares.map( action => {
+            if(action.pattern === fp) targetAction = action;
+            return action;
+        } );
+        if(!targetAction){
+            targetAction = {
+                pattern: fp,
+                callbacks: []
+            };
+            this.middlewares.push(targetAction);
         }
+        Array.prototype.push.apply(targetAction.callbacks, cbList);
         return this;
     },
     use(path, subRouter){
@@ -26,12 +35,19 @@ let routerPrototype = {
         subRouter.parent = this;
         if(!this.children) this.children = [];
         this.children.push(subRouter);
-        if(this.server){
-            subRouter.activeAll();
-        }
+
+        //if(this.server){
+        //    subRouter.activeAll();
+        //}
+
         return this;
     },
     setServer(server){
+        console.error("[Restify-routing] method 'setServer' is deprecated, but still valid. " +
+            "Please use 'applyRoutes' instead, to make code more readable");
+        return this.applyRoutes(server);
+    },
+    applyRoutes(server){
         this.server = server;
         this.setChildrenServer(server);
         if(this.server){
@@ -60,56 +76,75 @@ let routerPrototype = {
     activeAllPaths(){
         if(!this.server) return this;
         if(this.actions){
-            //for(let i=0; i<this.actions.length; i++) {
-            //    let param = this.actions[i];
-            //    this.active(param.method, param.path, param.callback);
-            //}
-
-            // Merge superActions to actions
-            if(this.superActions)
-                for(let path in this.superActions){
-                    let cbList = this.superActions[path];
-                    if(cbList.length > 0)
-                        for(let method of this.legalMethods){
-                            if(method === 'delete') continue;
-                            if(!this.actions) continue;
-                            if(!this.actions[method]) continue;
-                            if(!this.actions[method][path]) continue;
-                            for(let i = cbList.length - 1; i>=0; i--)
-                                this.actions[method][path].unshift(cbList[i]);
-                        }
-                }
-            delete this.superActions;
             // Apply routes
             for(let method in this.actions)
                 for(let path in this.actions[method]){
-                    let actions = this.actions[method][path];
-                    if(actions.length > 1) {
-                        this.active(method, path, actions);
-                    }else if(actions.length === 1){
-                        this.active(method, path, actions[0]);
-                    }
+                    this.active(method, path);
                 }
         }
         return this;
     },
+    allMiddlewares(){
+        let r = this;
+        let mwlist = [];
+        if(this.middlewares) {
+            mwlist.push.apply(mwlist, this.middlewares);
+            mwlist.forEach((action)=>{
+               action.absolutePattern = this.absolutePath(action.pattern);
+            });
+        }
+        while(r.parent) {
+            r = r.parent;
+            if(!r.middlewares) {
+                continue;
+            }
+            for(let i = r.middlewares.length - 1; i>=0; i--){
+                //if(this.isPatternCoverSubRoutes(r.middlewares[i].pattern)) {
+                let action = r.middlewares[i];
+                action.absolutePattern = r.absolutePattern(action.pattern);
+                mwlist.unshift(action);
+                //}
+            }
+        }
+        return mwlist;
+    },
+    actionsForMethodAtPath(method, path){
+        if(!method || !path) return null;
+        if(!this.actions || !this.actions[method] || !this.actions[method][path]) return null;
+        let cbList = this.actions[method][path];
+
+        // Get compatible middlewares
+        let mwlist = this.allMiddlewares();
+        for(let i = mwlist.length-1; i>=0; i--){
+            let action = mwlist[i];
+            if(this.isPathCompatibleWithPattern(this.absolutePath(path), action.absolutePattern)){
+                Array.prototype.unshift.apply(cbList, action.callbacks);
+            }
+        }
+
+        return cbList;
+    },
     active(method, path, cb){
-        if(!this.server || !path || !cb) return this;
-        let p = this.fullPath(path);
+        if(!this.server || !path ) return this;
+        let p = this.absolutePath(path);
         if(typeof cb === 'function')
             this.server[method](p, cb);
         else if(Array.isArray(cb)){
             let args = [p];
             args.push.apply(args, cb);
             this.server[method].apply(this.server, args);
+        }else{
+            let cbList = this.actionsForMethodAtPath(method, path);
+            if(!cbList || !Array.isArray(cbList) || cbList.length == 0) return this;
+            return this.active(method, path, cbList);
         }
 
         let m = method.toUpperCase();
         if(m === 'DEL') m =  'DELETE';
-        console.log('[Route] method: '+m+', path: '+p)
+        this.debugLog('[Route] method: '+m+', path: '+p)
         return this;
     },
-    fullPath(subPath){
+    absolutePath(subPath){
         let fp = this.path;
         let x = this.parent;
         while(x){
@@ -123,9 +158,42 @@ let routerPrototype = {
         }
         return fp;
     },
+    absolutePattern(pattern){
+        let p = pattern || '*';
+        let fp = this.absolutePath();
+        p = (fp +'/'+p).replace(/\/\//g, '/');
+        return p;
+
+    },
     translateMethod(m){
         if(m!=='delete') return m;
         else return 'del';
+    },
+    isPathCompatibleWithPattern(path, pattern){
+        if(!path || !pattern)return false;
+        const parts = pattern.split('*');
+        let eatenPath = path;
+        let isEndWithStarSyntax = false;
+        for(let part of parts){
+            if(part.length > 0){
+                if(eatenPath.indexOf(part)>=0){
+                    eatenPath = eatenPath.substr(eatenPath.indexOf(part) + part.length)
+                }else{
+                    return false;
+                }
+                isEndWithStarSyntax = false;
+            }else{
+                isEndWithStarSyntax = true;
+            }
+        }
+        if(eatenPath.length > 0 && !isEndWithStarSyntax) return false;
+
+        return true;
+    },
+    isPatternCoverSubRoutes(pattern){
+        if(!pattern || pattern.length === 0) return false;
+        if(pattern.substr(pattern.length-1, 1) === '*') return true;
+        return false;
     }
 };
 
@@ -135,30 +203,33 @@ for(let i=0; i< legalMethods.length; i++){
 }
 
 function setMethod(method){
-    let restifyMethod = method;
-    if(method==='delete') restifyMethod = 'del';
-    routerPrototype[method] = function(path, cb){
-        //if(!this.actions) this.actions = [];
-        //this.actions.push({
-        //    method: restifyMethod,
-        //    path: path,
-        //    callback: cb
-        //});
+    routerPrototype[method] = function(path){
         const m = this.translateMethod(method);
         if(!this.actions) this.actions = {};
         if(!this.actions[m]) this.actions[m] = {};
         if(!this.actions[m][path]) this.actions[m][path] = [];
-        this.actions[m][path].push(cb);
-        if(this.server){
-            this.active(this.translateMethod(restifyMethod), path, cb);
+        let cbList = Array.prototype.slice.call(arguments, 1);
+        if(cbList && cbList.length > 0){
+            Array.prototype.push.apply(this.actions[m][path], cbList);
         }
+        //if(this.server){
+        //    this.active(m, path);
+        //}
     }
 }
 
-function Router(serverInstance) {
-    if(serverInstance) this.server = serverInstance;
+function Router(params) {
+    this.server = null;
     this.path = '/';
     this.parent = null;
+    this.debug = false;
+    if(params && !params.server) {
+        this.server = params;
+    } else if(params){
+        for(let prop in params){
+            this[prop] = params[prop];
+        }
+    }
 }
 
 Router.prototype = routerPrototype;
@@ -167,12 +238,14 @@ Router.climbPathTree = function climbPathTree (tree, path){
     if(!tree)return null;
     let treePath = path || '/';
     let router = new Router();
-    if(tree.all){
-        if(typeof tree.all === 'function'){
-            router.all(treePath, tree.all);
-        }else if(Array.isArray(tree.all)){
-            let args = [treePath];
-            args.push.apply(args, tree.all);
+    if(tree.middlewares){
+        for(let action of tree.middlewares){
+            let args = [action.pattern];
+            if(action.callbacks && Array.isArray(action.callbacks)){
+                args.push.apply(args, action.callbacks);
+            }else if(typeof action.callbacks === 'function'){
+                args.push(action.callbacks);
+            }
             router.all.apply(router, args);
         }
     }
--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,14 @@
 'use strict'
 const request = require('supertest'),
-    Router = require('../index'),
+    should = require('should'),
     restify = require('restify'),
+    Router = require('../index'),
     server = restify.createServer({serverName: "Test restify-routing", serverVersion: "0.1.0"})
 
 server.use(restify.queryParser())
 server.use(restify.bodyParser())
 
-function preCheckForAllMethods(req, res, next){
+function middleware(req, res, next){
     req.count = (req.count)?req.count+1:1;
     return next()
 }
@@ -17,6 +18,15 @@ function fakeController (req, res){
     res.send(200, {count: req.count, path: req.route.path})
 }
 let pathTree = {
+    middlewares:[
+        {
+            pattern: '*',
+            callbacks: [
+                middleware,
+                middleware
+            ]
+        }
+    ],
     subPaths:{
         user: {
             subPaths:{
@@ -34,7 +44,14 @@ let pathTree = {
         service: {
             subPaths: {
                 orderSeat: {
-                    all : [ preCheckForAllMethods, preCheckForAllMethods ],
+                    //middlewares:[
+                    //    {
+                    //        pattern: '*',
+                    //        callbacks: [
+                    //            middleware
+                    //        ]
+                    //    }
+                    //],
                     allowedMethods: {
                         put : fakeController
                     },
@@ -83,39 +100,55 @@ let pathTree = {
 }
 
 let router = Router.climbPathTree(pathTree)
-router.setServer(server)
+router.applyRoutes(server)
 
-//server.listen(3000)
+// Test the logic of function 'isPathCompatibleWithPattern'
+describe('function isPathCompatibleWithPattern', function(){
+    let testCases = [
+        { path: '/service/orderMeal', pattern: '/*/order*', expect: true} ,
+        { path: '/service/orderMeal', pattern: '*', expect: true} ,
+        { path: '/service/orderMeal', pattern: '/*', expect: true} ,
+        { path: '/service/orderMeal', pattern: '/service/*', expect: true} ,
+        { path: '/service/orderMeal', pattern: '/service*', expect: true} ,
+        { path: '/service/orderMeal', pattern: '/service/ordermeal', expect: false} ,
+        { path: '/service/orderMeal', pattern: '/*/', expect: false} ,
+        { path: '/service/orderMeal', pattern: '*/', expect: false} ,
+        { path: '/service/orderMeal', pattern: '*service*', expect: true}
+    ];
+    for(let test of testCases) {
+        let exp = test.expect?'be':'NOT be';
+        it("path '"+ test.path + "' should "+ exp + " compatible with pattern '" +test.pattern +"'", function () {
+            router.isPathCompatibleWithPattern(test.path, test.pattern).should.be.exactly(test.expect);
+        });
+    }
+});
 
 // Test the path tree
 function shootFruits(tree, path){
     if(!tree)return;
     let fruitPath = path || '/';
 
-    if(tree.allowedMethods){
-        for(let method in tree.allowedMethods){
-            if(typeof tree.allowedMethods[method] === 'function') {
-                let pathInstance =  fruitPath.replace(/:[a-zA-Z0-9]+/g, Math.random())
-                request(server)[method]( pathInstance)
-                    .expect(200)
-                    .end(function(err, res){
-                        if(err) throw err;
-                        else {
-                            let expectedCount = 1;
-                            if(tree.all){
-                                if(typeof tree.all === 'function') expectedCount += 1;
-                                else expectedCount+=tree.all.length;
-                            }
-                            if(res.body.count != expectedCount){
-                                throw new Error('Routing did NOT go through the *all* method first, ' +
-                                    'expected response value is '+ expectedCount +', but actual response is ' +
-                                    res.body.count)
-                            }
-                        }
-                    })
+    describe('Route: '+path, function(){
+        if(tree.allowedMethods){
+            for(let method in tree.allowedMethods){
+                if(typeof tree.allowedMethods[method] === 'function') {
+                    let pathInstance =  fruitPath.replace(/:[a-zA-Z0-9]+/g, Math.random());
+                   it("response to ["+method.toUpperCase() +"] path '" + pathInstance + "' should be 200", function(done){
+                       request(server)[method]( pathInstance)
+                           .expect(200)
+                           .expect(function(res){
+                               res.body.count.should.be.exactly(3);
+                           })
+                           .end(function(err){
+                               if(err) return done(err);
+                               else done();
+                           })
+                   })
+                }
             }
         }
-    }
+    });
+
     if(tree.subPaths){
         for(let subPath in tree.subPaths){
             let targetPath = fruitPath + '/' + subPath;
